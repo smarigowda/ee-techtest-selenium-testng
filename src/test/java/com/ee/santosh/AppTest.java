f3f8fead8f32f4250a8d55d99c996008500381ff
Santosh Marigowda <santosharakere@gmail.com>
2020-05-29 12:00:34 +0100

    ready for review

--- src/test/java/com/ee/santosh/AppTest.java
+++ src/test/java/com/ee/santosh/AppTest.java
@@ -1,42 +1,38 @@
 package com.ee.santosh;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
+
+
 public class AppTest {
+    final String url = "http://hotel-test.equalexperts.io/";
     @Test
-    public void Test1() throws InterruptedException {
-        //setup the chromedriver using WebDriverManager
+    public void Test_1_book_with_deposit_true() throws Exception {
         WebDriverManager.chromedriver().setup();
 
-        //Create driver object for Chrome
         WebDriver driver = new ChromeDriver();
-        // Setup an implicit wait of 10 sec
+        driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         HomePage homePage = new HomePage(driver);
+
         homePage
-                .open("http://hotel-test.equalexperts.io/")
-                .setCheckinDate("2020-05-20")
+                .open(url)
+                .deleteOrder() // cleanup task, may be this has to go into before block
                 .setCheckoutDate("2020-05-28")
                 .setFirstName("Santosh")
                 .setLastName("Marigowda")
-                .setTotalPrice("100");
+                .setTotalPrice("100")
+                .setDeposit("false")
+                .setCheckinDate("2020-05-20")
+                .saveBooking();
 
-        Select depositSelector = new Select(driver.findElement(By.cssSelector("#depositpaid")));
-        depositSelector.selectByVisibleText("true");
-        Thread.sleep(5000);
-
-        //quit the browser
+        // Just to see the final result visually, will be removed in real test
+        Thread.sleep(3000);
         driver.quit();
     }
 }
--- src/test/java/com/ee/santosh/HomePage.java
+++ src/test/java/com/ee/santosh/HomePage.java
@@ -1,9 +1,8 @@
 package com.ee.santosh;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
@@ -15,29 +14,67 @@ public class HomePage {
     String surnameSelector = "#lastname";
     String totalpriceSelector = "#totalprice";
     String depositpaidSelector = "#depositpaid";
+    String saveButtonSelector = "input[type=button]";
+    String deleteButtonSelector = "input[value=Delete]";
 
     HomePage(WebDriver driver) {
         this.driver = driver;
     }
-    public HomePage open(String url) {
-        //Navigate to a URL
-        driver.get(url);
-        // Wait until the page shows up
-        WebDriverWait wait = new WebDriverWait(this.driver, 10);
-        // Explicitly wait for the heading to be present on the page
-        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(this.headingSelector)));
+
+    public HomePage deleteOrder() {
+        WebDriverWait wait = new WebDriverWait(this.driver, 2);
+        try {
+            WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(this.deleteButtonSelector)));
+            element.click();
+        } catch (Exception e) {
+            System.out.println("Order not found. Not to worry.");
+        }
         return this;
     }
 
-    public HomePage setCheckinDate(String checkinDate) {
+    private void waitForJS(String jsCode, String expectedValue) throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        for(int i=0; i < 10; i++) {
+            String sText =  js.executeScript(jsCode).toString();
+            if(sText.equals(expectedValue)) {
+                break;
+            }
+            System.out.println(sText);
+            Thread.sleep(2000);
+        }
+    }
+
+    private HomePage waitForOrderToBeDisplayed() throws Exception {
+        WebDriverWait wait = new WebDriverWait(this.driver, 15);
+        try {
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(this.deleteButtonSelector)));
+        } catch (Exception e) {
+            System.out.println("Order not found. This is an issue to be worried about.");
+            throw new Exception("Unable to save the order. Please check the log");
+        }
+        return this;
+    }
+
+    public HomePage open(String url) throws InterruptedException {
+        driver.get(url);
+        WebDriverWait wait = new WebDriverWait(this.driver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(this.headingSelector)));
+        return this;
+    }
+
+    public HomePage setCheckinDate(String checkinDate) throws InterruptedException {
         WebElement dateBox = driver.findElement(By.cssSelector(this.checkinDateSelector));
         dateBox.sendKeys(checkinDate);
+        // now get the checkInDate and wait until it is set to expected value
+        this.waitForJS("return document.getElementById('checkin').value;", checkinDate);
+        dateBox.sendKeys(Keys.ENTER);
         return this;
     }
 
-    public HomePage setCheckoutDate(String checkoutDate) {
+    public HomePage setCheckoutDate(String checkoutDate) throws InterruptedException {
         WebElement dateBox = driver.findElement(By.cssSelector(this.checkoutDateSelector));
         dateBox.sendKeys(checkoutDate);
+        this.waitForJS("return document.getElementById('checkout').value;", checkoutDate);
         return this;
     }
 
@@ -57,5 +94,18 @@ public class HomePage {
         WebElement element = driver.findElement(By.cssSelector(this.totalpriceSelector));
         element.sendKeys(totalPrice);
         return this;
+    }
+
+    public HomePage setDeposit(String isPaid) {
+        Select depositSelector = new Select(driver.findElement(By.cssSelector(this.depositpaidSelector)));
+        depositSelector.selectByVisibleText(isPaid);
+        return this;
+    }
+
+    public void saveBooking() throws Exception {
+        WebElement element = driver.findElement(By.cssSelector(this.saveButtonSelector));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
+        element.click();
+        this.waitForOrderToBeDisplayed();
     }
 }
